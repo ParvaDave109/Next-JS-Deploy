bf88a2b8e89d2030f043769532b9e48c417f4afe
parvadave109 <parvadave109>
2023-05-02 12:32:19 +0530

    Added Next Js Application

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -41,7 +41,7 @@ export async function getStaticPaths(){
 }
 
 export async function getStaticProps(context){
-    const client = await MongoClient.connect('mongodb://localhost:27017/Meetups');
+    const client = await MongoClient.connect('mongodb+srv://parvadave109:HIHTX9teRMODop3O@cluster0.371ysxq.mongodb.net/Meetups?retryWrites=true&w=majority');
     const db = client.db();
     const meetupsCollection = db.collection('meetups');
     const meetup = await meetupsCollection.findOne({'_id':new ObjectId(context.params.meetupId)});
